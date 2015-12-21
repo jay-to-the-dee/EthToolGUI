86f054df2db50d80774c186d37b635008628f05b
jay-to-the-dee <jay-to-the-dee@users.noreply.github.com>
2015-12-21 22:10:21 +0000

    AbstractLinkModes development

--- src/model/property/AbstractLinkModes.java
+++ src/model/property/AbstractLinkModes.java
@@ -26,30 +26,30 @@ public abstract class AbstractLinkModes extends MultiEnumProperty
 {
     public enum LinkModesEnum
     {
-        ADVERTISED_10baseT_Half, ADVERTISED_10baseT_Full, ADVERTISED_100baseT_Half, ADVERTISED_100baseT_Full, ADVERTISED_1000baseT_Half, ADVERTISED_1000baseT_Full, ADVERTISED_1000baseKX_Full, ADVERTISED_2500baseX_Full, ADVERTISED_10000baseT_Full, ADVERTISED_10000baseKX4_Full, ADVERTISED_10000baseKR_Full, ADVERTISED_20000baseMLD2_Full, ADVERTISED_20000baseKR2_Full, ADVERTISED_40000baseKR4_Full, ADVERTISED_40000baseCR4_Full, ADVERTISED_40000baseSR4_Full, ADVERTISED_40000baseLR4_Full
+        LM_10baseT_Half, LM_10baseT_Full, LM_100baseT_Half, LM_100baseT_Full, LM_1000baseT_Half, LM_1000baseT_Full, LM_1000baseKX_Full, LM_2500baseX_Full, LM_10000baseT_Full, LM_10000baseKX4_Full, LM_10000baseKR_Full, LM_20000baseMLD2_Full, LM_20000baseKR2_Full, LM_40000baseKR4_Full, LM_40000baseCR4_Full, LM_40000baseSR4_Full, LM_40000baseLR4_Full
     }
 
-    public AbstractLinkModes()
+    public AbstractLinkModes(String propertyName, boolean readOnly)
     {
-        super("Port", true);
+        super(propertyName, readOnly);
         enumToStrings = new EnumMap(LinkModesEnum.class);
         //Taken from line 521 onwards in ethtool.c
-        enumToStrings.put(LinkModesEnum.ADVERTISED_10baseT_Half, "10baseT/Half");
-        enumToStrings.put(LinkModesEnum.ADVERTISED_10baseT_Full, "10baseT/Full");
-        enumToStrings.put(LinkModesEnum.ADVERTISED_100baseT_Half, "100baseT/Half");
-        enumToStrings.put(LinkModesEnum.ADVERTISED_100baseT_Full, "100baseT/Full");
-        enumToStrings.put(LinkModesEnum.ADVERTISED_1000baseT_Half, "1000baseT/Half");
-        enumToStrings.put(LinkModesEnum.ADVERTISED_1000baseT_Full, "1000baseT/Full");
-        enumToStrings.put(LinkModesEnum.ADVERTISED_1000baseKX_Full, "1000baseKX/Full");
-        enumToStrings.put(LinkModesEnum.ADVERTISED_2500baseX_Full, "2500baseX/Full");
-        enumToStrings.put(LinkModesEnum.ADVERTISED_10000baseT_Full, "10000baseT/Full");
-        enumToStrings.put(LinkModesEnum.ADVERTISED_10000baseKX4_Full, "10000baseKX4/Full");
-        enumToStrings.put(LinkModesEnum.ADVERTISED_10000baseKR_Full, "10000baseKR/Full");
-        enumToStrings.put(LinkModesEnum.ADVERTISED_20000baseMLD2_Full, "20000baseMLD2/Full");
-        enumToStrings.put(LinkModesEnum.ADVERTISED_20000baseKR2_Full, "20000baseKR2/Full");
-        enumToStrings.put(LinkModesEnum.ADVERTISED_40000baseKR4_Full, "40000baseKR4/Full");
-        enumToStrings.put(LinkModesEnum.ADVERTISED_40000baseCR4_Full, "40000baseCR4/Full");
-        enumToStrings.put(LinkModesEnum.ADVERTISED_40000baseSR4_Full, "40000baseSR4/Full");
-        enumToStrings.put(LinkModesEnum.ADVERTISED_40000baseLR4_Full, "40000baseLR4/Full");
+        enumToStrings.put(LinkModesEnum.LM_10baseT_Half, "10baseT/Half");
+        enumToStrings.put(LinkModesEnum.LM_10baseT_Full, "10baseT/Full");
+        enumToStrings.put(LinkModesEnum.LM_100baseT_Half, "100baseT/Half");
+        enumToStrings.put(LinkModesEnum.LM_100baseT_Full, "100baseT/Full");
+        enumToStrings.put(LinkModesEnum.LM_1000baseT_Half, "1000baseT/Half");
+        enumToStrings.put(LinkModesEnum.LM_1000baseT_Full, "1000baseT/Full");
+        enumToStrings.put(LinkModesEnum.LM_1000baseKX_Full, "1000baseKX/Full");
+        enumToStrings.put(LinkModesEnum.LM_2500baseX_Full, "2500baseX/Full");
+        enumToStrings.put(LinkModesEnum.LM_10000baseT_Full, "10000baseT/Full");
+        enumToStrings.put(LinkModesEnum.LM_10000baseKX4_Full, "10000baseKX4/Full");
+        enumToStrings.put(LinkModesEnum.LM_10000baseKR_Full, "10000baseKR/Full");
+        enumToStrings.put(LinkModesEnum.LM_20000baseMLD2_Full, "20000baseMLD2/Full");
+        enumToStrings.put(LinkModesEnum.LM_20000baseKR2_Full, "20000baseKR2/Full");
+        enumToStrings.put(LinkModesEnum.LM_40000baseKR4_Full, "40000baseKR4/Full");
+        enumToStrings.put(LinkModesEnum.LM_40000baseCR4_Full, "40000baseCR4/Full");
+        enumToStrings.put(LinkModesEnum.LM_40000baseSR4_Full, "40000baseSR4/Full");
+        enumToStrings.put(LinkModesEnum.LM_40000baseLR4_Full, "40000baseLR4/Full");
     }
 }
